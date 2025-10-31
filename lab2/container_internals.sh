53a1725463ce51907c67eb44296cc4ad02d29fa2
Konstantin Skibin <Konst.SKI@yandex.ru>
2025-10-31 11:36:02 +0700

    done

---
+++ lab2/container_internals.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+container_code=$HOSTNAME
+counter=1
+shared_dir=/app/shared
+mkdir -p $shared_dir
+
+lock_file=$shared_dir/.lockfile
+touch $lock_file
+
+exec 404>"$lock_file" # Ставим в соответствие файлу lockfile дескриптор 202
+
+while :; do
+	created=""
+	flock -x 404
+	i=1
+	while [ $i -le 999 ]; do
+		filename=$(printf %03d $i)
+		path=$shared_dir/$filename
+		if [ ! -e "$path" ]; then
+			counter=$((counter+1))
+            touch $path
+			echo "$container_code $counter" > "$path"
+			created="$filename"
+			break
+		fi
+		i=$((i+1))
+	done
+	
+	flock -u 404
+	
+	if [ -n "$created" ]; then
+		sleep 1
+		rm "$shared_dir/$created"
+		sleep 1
+	else
+		sleep 2
+	fi
+done
---
+++ lab2/run.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+# Первый аргумент вызова - название docker volume
+# Второй аргумент - количество одновременно запущенных контейнеров
+
+vol_name=$1;  # Наименование тома
+container_num=$2; # Количество контейнеров
+
+exit_handler()
+{
+    return_code=$?
+    trap - EXIT INT HUP QUIT TERM
+    echo "Остановка контейнеров"
+    i=1
+    while [ $i -le $container_num ]; do
+        docker stop "container_$i"
+        i=$((i+1))
+    done
+    docker rmi lab2_unix
+    exit $return_code
+}
+
+trap exit_handler INT HUP QUIT TERM EXIT
+
+docker build -t lab2_unix . || exit 1
+i=1;
+while [ $i -le $container_num ]; do
+    docker run -d --rm --name "container_$i" --volume $vol_name:/app/shared lab2_unix || exit 2
+    i=$((i+1));
+done
+
+while :; do
+{
+    sleep 200
+}
+done
